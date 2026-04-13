aaad1336f0a75887be897a2ed2cfe0e87d377380
Alberto Giuntelli <alberto.giuntelli@gmail.com>
2026-04-13 12:30:15 +0200

    Fix AdminLayout: ripristinato Outlet, layout e pagine admin

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,13 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import "./AdminLayout.css";
 
-export default function AdminLayout({ children }) {
+export default function AdminLayout() {
     return (
         <div className="admin-layout">
             <Sidebar />
 
             <div className="admin-content">
-                {children}
+                <Outlet />
             </div>
         </div>
     );
