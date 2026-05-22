1177e76d93a2dcaee953b85e534507d4359fce67
Alberto Giuntelli <alberto.giuntelli@gmail.com>
2026-05-22 17:00:49 +0200

    Fix baseURL axios (rimosso /api)

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://backend-nuova-production.up.railway.app/api",
+    baseURL: "https://backend-nuova-production.up.railway.app",
     headers: {
         "Content-Type": "application/json"
     }
